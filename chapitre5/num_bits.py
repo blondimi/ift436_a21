81c3d6a9f2bcbefc624e3d2ce9ee30a6ccd1e35d
Michael Blondin <michael.blondin@tum.de>
2021-11-09 08:19:58 -0500

    Sol. alternative

--- chapitre5/num_bits.py
+++ chapitre5/num_bits.py
@@ -14,6 +14,23 @@ def num_bits(s):
 
     return (len(s) - pos) if pos is not None else 0
 
+def num_bits_alternatif(s):
+    def premier_un(lo, hi):
+        if lo == hi:
+            return lo
+        else:
+            mid = (lo + hi) // 2
+
+            if s[mid] == 0:
+                return premier_un(mid + 1, hi)
+            elif s[mid] == 1:
+                return premier_un(lo, mid)
+
+    if len(s) == 0 or s[-1] == 0:
+        return 0
+    else:
+        return len(s) - premier_un(0, len(s) - 1)
+
 # Exemple des notes de cours
 if __name__ == "__main__":
     s = [0, 0, 0, 1, 1, 1, 1, 1]
@@ -21,3 +38,4 @@ if __name__ == "__main__":
     print("Séquence s =", s)
     print()
     print("Nombre de bits égaux à 1:", num_bits(s))
+    print("Nombre de bits égaux à 1:", num_bits_alternatif(s))
